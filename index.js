bb1dd4b0e72725b92a7cf3efcebe47ebd0495738
Jason <jason.nordheim@gmail.com>
2021-01-18 08:16:55 -0700

    imported `passport` and `passport-google-auth20` strategy

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const passport = require('passport') // passport library 
+const GoogleStrategy = require('passport-google-oauth20').Strategy // strategy for passport 
+
 const app = express()
+
+passport.use(new GoogleStrategy())
 
 app.get('/', (req, res) => {
     res.send({ test: 'Is this working?' })
