d5db77220b0d80992deb6b88f7c228eadd630eaf
Jason <jason.nordheim@gmail.com>
2021-01-18 10:59:14 -0700

    changed strategy options, adding in the tokenURL and callbackURL from google config

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express')
 const passport = require('passport') // passport library 
 const GoogleStrategy = require('passport-google-oauth20').Strategy // strategy for passport 
 
+const keys = require('./config/keys')
+
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy({
+    clientID: keys.web.client_id, // public token 
+    clientSecret: keys.web.client_secret, // private token 
+    tokenURL: keys.web.token_uri,
+    callbackURL: keys.web.callbackURL,
+}, (accessToken) => {
+    console.log(accessToken)
+}))
 
-app.get('/', (req, res) => {
-    res.send({ test: 'Is this working?' })
-})
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 
 const PORT = process.env.PORT || 5000
 
