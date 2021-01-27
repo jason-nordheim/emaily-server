1d92a70bbe8f6d47ceb2d60e69773dea0106366b
jason-nordheim <jason.nordheim@gmail.com>
2021-01-27 13:43:58 -0700

    corrected typo

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
   web: {
     client_id: process.env.GOOGLE_CLIENT_ID,
     client_secret: process.env.GOOGLE_CLIENT_SECRET,
-    redirect_uri = [`${process.env.REDIRECT_URI}`], 
+    redirect_uris = [process.env.REDIRECT_URI], 
   },
     mongoAtlas: {
       uri: process.env.MONGO_URI,
