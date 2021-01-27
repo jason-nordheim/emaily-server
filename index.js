6e9d86cfbf01ebf7dc4d19826124401b174af8ea
jason-nordheim <jason.nordheim@gmail.com>
2021-01-27 13:26:22 -0700

    wrapped listen in try catch

--- index.js
+++ index.js
@@ -39,5 +39,8 @@ const PORT = process.env.PORT || 5000;
 // const HOST_NAME = process.env.PORT
 //   ? "sheltered-citadel-72082.herokuapp.com"
 //   : "localhost";
-
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+try {
+  app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+} catch (ex) {
+  console.error(ex);
+}
