3c9dfcc6e25419a2c0315d53ec289ad54ec87c98
jason-nordheim <jason.nordheim@gmail.com>
2021-01-27 13:24:33 -0700

    removed HOST_NAME declaration

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ require("./routes/authRoutes")(app);
 // });
 /** start server */
 const PORT = process.env.PORT || 5000;
-const HOST_NAME = process.env.PORT
-  ? "sheltered-citadel-72082.herokuapp.com"
-  : "localhost";
+// const HOST_NAME = process.env.PORT
+//   ? "sheltered-citadel-72082.herokuapp.com"
+//   : "localhost";
 
-app.listen(PORT, HOST_NAME, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
