9d3bebd674399a32eb81457e669fe7330fa32efa
jason-nordheim <jason.nordheim@gmail.com>
2021-01-27 12:12:04 -0700

    created login and logout routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,31 @@
-const passport = require('passport');
+const passport = require("passport");
 
 module.exports = (app) => {
-    /**
-     * Auth & Passport 
-     * o Auth route handler using passport 
-     */
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }));
+  /**
+   * Auth & Passport
+   * o Auth route handler using passport
+   */
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
 
-    /**
-     * Auth & Passport 
-     * use code received from google to retrieve user profile 
-     */
-    app.get('/auth/google/callback', passport.authenticate('google'));
+  /**
+   * Auth & Passport
+   * use code received from google to retrieve user profile
+   */
+  app.get("/auth/google/callback", passport.authenticate("google"));
+
+  /** add route to retrieve the current user from cookies */
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+
+  /** add route to logout user  */
+  app.get("/api/logout", (req, res) => {
+    req.logout(); // removes the user id from the cookie
+    res.send(req.user); // send response (should respond with a nulled user object)
+  });
 };
-
