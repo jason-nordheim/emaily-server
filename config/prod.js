f359ac7c7779bac2d3fcda055d028c4ee8c32a8e
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 09:04:19 -0700

    tried moving retrieval of process.env.REDIRECT_URI into a const before passing it to the config object

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,10 @@
 // production keys
+const redirect_uri = process.env.REDIRECT_URI
 module.exports = {
   web: {
     client_id: process.env.GOOGLE_CLIENT_ID,
     client_secret: process.env.GOOGLE_CLIENT_SECRET,
-    redirect_uris = [process.env.REDIRECT_URI], 
+    redirect_uris = [redirect_uri], 
   },
     mongoAtlas: {
       uri: process.env.MONGO_URI,
